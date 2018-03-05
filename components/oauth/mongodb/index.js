9961c0d31609f0a3d2ac2d50701672dfdcf6a73f
Achille Nana Chimi <nanachimi@kumity.com>
2018-03-05 07:59:40 +0100

    new mongoose setup added

--- components/oauth/mongodb/index.js
+++ components/oauth/mongodb/index.js
@@ -3,20 +3,26 @@
  */
 
 /** https://github.com/dsquier/oauth2-server-php-mysql **/
-var config = require('./../../../config')
-var mongoose = require('mongoose');
-console.log(config.mongo.uri)
-mongoose.connect(config.mongo.uri, function(err) {
-  if (err) return console.log(err);
-  console.log('Mongoose Connected');
-});
-var db ={};
-db.OAuthAccessToken = require('./OAuthAccessToken')
-db.OAuthAuthorizationCode = require('./OAuthAuthorizationCode')
-db.OAuthClient = require('./OAuthClient')
-db.OAuthRefreshToken = require('./OAuthRefreshToken')
-db.OAuthScope = require('./OAuthScope')
-db.User = require('./User')
-db.Thing = require('./Thing')
+var config = require("./../../../config");
+var mongoose = require("mongoose");
+var Promise = require("bluebird");
+console.log(config.mongo.uri);
+mongoose.Promise = Promise;
+mongoose.connect(
+  config.mongo.uri,
+  { keepAlive: true, useMongoClient: true },
+  function(err) {
+    if (err) return console.log(err);
+    console.log("Mongoose Connected");
+  }
+);
+var db = {};
+db.OAuthAccessToken = require("./OAuthAccessToken");
+db.OAuthAuthorizationCode = require("./OAuthAuthorizationCode");
+db.OAuthClient = require("./OAuthClient");
+db.OAuthRefreshToken = require("./OAuthRefreshToken");
+db.OAuthScope = require("./OAuthScope");
+db.User = require("./User");
+db.Thing = require("./Thing");
 
 module.exports = db;
